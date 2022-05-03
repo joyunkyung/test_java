0e0e2c735078dee0c88ac0f9155934aa73fd2070
joyunkyung <dbsrud458@naver.com>
2022-05-03 18:23:01 +0900

    Hello

---
+++ src/git_test/Git_test.java
@@ -0,0 +1,17 @@
+package git_test;
+
+public class Git_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		var hour = 3;
+		var minute = 5;
+		var second = 10;
+		
+		System.out.println(hour + "시간 "+ minute + "분 "+ second + "초");
+		
+
+	}
+
+}
